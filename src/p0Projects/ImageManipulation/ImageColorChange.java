bf2259cfe5e2f0cfc02b894f70eed14fd8f78506
Siddhant Vij <siddhantvij201@gmail.com>
2023-09-02 19:49:21 +0530

    Image Color Change - Latency

---
+++ src/p0Projects/ImageManipulation/ImageColorChange.java
@@ -0,0 +1,141 @@
+package p0Projects.ImageManipulation;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
+
+class ColorChange {
+  public static void recolorMultithreaded(BufferedImage originalImage, BufferedImage resultImage,
+      int numberOfThreads) {
+    List<Thread> threads = new ArrayList<>();
+    int width = originalImage.getWidth();
+    int height = originalImage.getHeight() / numberOfThreads;
+
+    for (int i = 0; i < numberOfThreads; i++) {
+      final int threadMultiplier = i;
+
+      Thread thread = new Thread(() -> {
+        int xOrigin = 0;
+        int yOrigin = height * threadMultiplier;
+
+        recolorImage(originalImage, resultImage, xOrigin, yOrigin, width, height);
+      });
+
+      threads.add(thread);
+    }
+
+    for (Thread thread : threads) {
+      thread.start();
+    }
+
+    for (Thread thread : threads) {
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+      }
+    }
+  }
+
+  public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
+    recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+  }
+
+  public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner,
+      int width, int height) {
+    for (int x = leftCorner; x < leftCorner + width && x < originalImage.getWidth(); x++) {
+      for (int y = topCorner; y < topCorner + height && y < originalImage.getHeight(); y++) {
+        recolorPixel(originalImage, resultImage, x, y);
+      }
+    }
+  }
+
+  public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y) {
+    int rgb = originalImage.getRGB(x, y);
+
+    int red = getRed(rgb);
+    int green = getGreen(rgb);
+    int blue = getBlue(rgb);
+
+    int newRed;
+    int newGreen;
+    int newBlue;
+
+    if (isShadeOfGray(red, green, blue)) {
+      newRed = Math.min(255, red + 10);
+      newGreen = Math.max(0, green - 80);
+      newBlue = Math.max(0, blue - 20);
+    } else {
+      newRed = red;
+      newGreen = green;
+      newBlue = blue;
+    }
+    int newRGB = createRGBFromColors(newRed, newGreen, newBlue);
+    setRGB(resultImage, x, y, newRGB);
+  }
+
+  public static void setRGB(BufferedImage image, int x, int y, int rgb) {
+    image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
+  }
+
+  public static boolean isShadeOfGray(int red, int green, int blue) {
+    return Math.abs(red - green) < 30 && Math.abs(red - blue) < 30 && Math.abs(green - blue) < 30;
+  }
+
+  public static int createRGBFromColors(int red, int green, int blue) {
+    int rgb = 0;
+
+    rgb |= blue;
+    rgb |= green << 8;
+    rgb |= red << 16;
+
+    rgb |= 0xFF000000;
+
+    return rgb;
+  }
+
+  public static int getRed(int rgb) {
+    return (rgb & 0x00FF0000) >> 16;
+  }
+
+  public static int getGreen(int rgb) {
+    return (rgb & 0x0000FF00) >> 8;
+  }
+
+  public static int getBlue(int rgb) {
+    return rgb & 0x000000FF;
+  }
+}
+
+public class ImageColorChange {
+  public static final String SOURCE_FILE = "./src/p0Projects/ImageManipulation/many-flowers.jpg";
+  public static final String DESTINATION_FILE_ST = "./src/p0Projects/ImageManipulation/many-flowers-output-0.jpg";
+  public static final String DESTINATION_FILE_PT = "./src/p0Projects/ImageManipulation/many-flowers-output-1.jpg";
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
+    BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(),
+        BufferedImage.TYPE_INT_RGB);
+
+    long startTime = System.currentTimeMillis();
+    ColorChange.recolorSingleThreaded(originalImage, resultImage);
+    long endTime = System.currentTimeMillis();
+    long duration = endTime - startTime;
+    File outputFile = new File(DESTINATION_FILE_ST);
+    ImageIO.write(resultImage, "jpg", outputFile);
+    System.out.println(String.valueOf(duration));
+
+    int numberOfThreads = Runtime.getRuntime().availableProcessors();
+    startTime = System.currentTimeMillis();
+    ColorChange.recolorMultithreaded(originalImage, resultImage, numberOfThreads);
+    endTime = System.currentTimeMillis();
+    duration = endTime - startTime;
+    outputFile = new File(DESTINATION_FILE_PT);
+    ImageIO.write(resultImage, "jpg", outputFile);
+    System.out.println(String.valueOf(duration));
+  }
+
+}
