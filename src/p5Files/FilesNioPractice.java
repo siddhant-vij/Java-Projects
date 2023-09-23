feaed95a8a8abe647031a4dac13b2c6183375339
Siddhant Vij <siddhantvij201@gmail.com>
2023-09-23 13:24:04 +0530

    Files IO & NIO Practice

--- src/p5Files/FilesNioPractice.java
+++ src/p5Files/FilesNioPractice.java
@@ -1,113 +1,83 @@
 package p5Files;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.file.StandardOpenOption;
 
 public class FilesNioPractice {
 
   public static void main(String[] args) throws IOException {
-    // Reads from the console the path to a file, reads lines from the file, and
-    // displays them on the screen.
-    Scanner sc = null;
-    BufferedReader reader = null;
-    try {
-      sc = new Scanner(System.in);
-      reader = Files.newBufferedReader(Path.of(sc.nextLine()));
-      String lineX;
-      while ((lineX = reader.readLine()) != null) {
-        System.out.println(lineX);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (sc != null)
-        sc.close();
-      if (reader != null)
-        reader.close();
+    // Get file name from the user - via console
+    String fileName;
+    System.out.print("Enter file name: ");
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+      fileName = reader.readLine();
     }
 
-    // Read the path to file1 and the path to file2 from the console.
-    // Write all the bytes from file1 to file2,
-    // but in doing so swap them according to this rule: swap the first with the
-    // second, the third with the fourth, and so on.
-    // If the last byte in file1 is odd, then write it to file2 as is.
-    InputStream fileInputStream = null;
-    OutputStream fileOutputStream = null;
-    try {
-      sc = new Scanner(System.in);
-      fileInputStream = Files.newInputStream(Paths.get(sc.nextLine()));
-      fileOutputStream = Files.newOutputStream(Paths.get(sc.nextLine()));
-      int fileSize = fileInputStream.available();
-      byte[] buffer = new byte[fileSize];
-      fileInputStream.read(buffer);
-      int bufferCounter = 0;
-      while (bufferCounter < fileSize - 1) {
-        byte byte1 = buffer[bufferCounter];
-        byte byte2 = buffer[bufferCounter + 1];
-        buffer[bufferCounter] = byte2;
-        buffer[bufferCounter + 1] = byte1;
-        fileOutputStream.write(buffer, bufferCounter, 2);
-        bufferCounter += 2;
-      }
-      fileOutputStream.write(buffer, bufferCounter, fileSize - bufferCounter);
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (sc != null)
-        sc.close();
-      if (fileInputStream != null)
-        fileInputStream.close();
-      if (fileOutputStream != null)
-        fileOutputStream.close();
+    // Reading a file via a Channel - Manual Buffer Creation
+    try (SeekableByteChannel fchan = Files.newByteChannel(Path.of(fileName))) {
+      ByteBuffer buffer = ByteBuffer.allocate(128);
+      int count = 0;
+      do {
+        count = fchan.read(buffer);
+        if (count != -1) {
+          buffer.rewind();
+          for (int i = 0; i < count; i++) {
+            System.out.print((char) buffer.get());
+          }
+        }
+      } while (count != -1);
+      System.out.println();
     }
 
-    // Read the name of a text file from the console
-    // Then read characters from this file (use the readAllLines(Path) method of
-    // the Files class) and displays everything except periods, commas, and spaces.
-    try {
-      sc = new Scanner(System.in);
-      String fileName = sc.nextLine();
-      List<String> linesX = Files.readAllLines(Paths.get(fileName));
-      for (String lineX : linesX) {
-        System.out.println(lineX.replaceAll("[,. ]", ""));
+    // Reading a file via a Channel - Buffer Map to Channel
+    try (FileChannel fchan = (FileChannel) Files.newByteChannel(Path.of(fileName))) {
+      long fSize = fchan.size();
+      MappedByteBuffer buffer = fchan.map(FileChannel.MapMode.READ_ONLY, 0, fSize);
+      for (int i = 0; i < fSize; i++) {
+        System.out.print((char) buffer.get());
       }
-    } finally {
-      if (sc != null)
-        sc.close();
+      System.out.println();
     }
 
-    // Read a file name from the console. Read a set of numbers from the file.
-    // Display only the even numbers, sorted in ascending order.
-    BufferedReader br = null;
-    List<Integer> list = new ArrayList<>();
-    try {
-      sc = new Scanner(System.in);
-      br = new BufferedReader(new InputStreamReader(new FileInputStream(sc.nextLine())));
-      String line;
-      while ((line = br.readLine()) != null) {
-        int num = Integer.parseInt(line);
-        if (num % 2 == 0)
-          list.add(num);
-      }
-    } finally {
-      if (sc != null)
-        sc.close();
-      if (br != null)
-        br.close();
+    String testText = "This is a test text. Could also be text taken from a file: line-by-line. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta erat ut diam condimentum vehicula. Ut rutrum risus dignissim metus vulputate consectetur. Sed urna purus, suscipit in nibh vitae, ultricies mollis nisl. Aliquam erat volutpat. Aliquam a gravida turpis. Ut vel eros pellentesque, pulvinar lorem at, hendrerit nunc. Morbi interdum vestibulum aliquet. Etiam sed maximus eros. In dolor nisi, lobortis at rutrum sed, rutrum a lectus.";
+
+    // Writing to a file via a Channel - Manual Buffer Creation
+    try (FileChannel fchan = (FileChannel) Files.newByteChannel(Path.of(fileName), StandardOpenOption.WRITE,
+        StandardOpenOption.CREATE)) {
+      ByteBuffer buffer = ByteBuffer.allocate(20);
+      int counter = 0;
+      do {
+        buffer.clear();
+        int bytesToRead = Math.min(buffer.capacity(), testText.length() - counter);
+        if (bytesToRead <= 0)
+          break; // Exit if there's nothing left to read
+
+        buffer.put(testText.getBytes(), counter, bytesToRead);
+
+        buffer.flip();
+        int writtenBytes = fchan.write(buffer);
+        counter += writtenBytes;
+      } while (counter < testText.length());
     }
-    Collections.sort(list);
-    for (int i = 0; i < list.size(); i++)
-      System.out.println(list.get(i));
+
+    // Writing to a file via a Channel - Buffer Map to Channel
+    try (FileChannel fchan = (FileChannel) Files.newByteChannel(Path.of(fileName), StandardOpenOption.WRITE,
+        StandardOpenOption.READ,
+        StandardOpenOption.CREATE)) {
+      long fSize = testText.length();
+      MappedByteBuffer buffer = fchan.map(FileChannel.MapMode.READ_WRITE, 0, fSize);
+      byte[] bytes = testText.getBytes();
+      for (int i = 0; i < fSize; i++) {
+        buffer.put(bytes[i]);
+      }
+    }
   }
 }
--- src/p5Files/FilesPractice.java
+++ src/p5Files/FilesPractice.java
@@ -2,13 +2,23 @@ package p5Files;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -218,5 +228,98 @@ public class FilesPractice {
             }
             fileWriter3.write(sb.toString(), 3 * fileLength / 4, fileLength - 3 * fileLength / 4);
         }
+
+        // Reads from the console the path to a file, reads lines from the file, and
+        // displays them on the screen.
+        Scanner sc = null;
+        BufferedReader reader = null;
+        try {
+            sc = new Scanner(System.in);
+            reader = Files.newBufferedReader(Path.of(sc.nextLine()));
+            String lineX;
+            while ((lineX = reader.readLine()) != null) {
+                System.out.println(lineX);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (sc != null)
+                sc.close();
+            if (reader != null)
+                reader.close();
+        }
+
+        // Read the path to file1 and the path to file2 from the console.
+        // Write all the bytes from file1 to file2,
+        // but in doing so swap them according to this rule: swap the first with the
+        // second, the third with the fourth, and so on.
+        // If the last byte in file1 is odd, then write it to file2 as is.
+        InputStream fileInputStream = null;
+        OutputStream fileOutputStream = null;
+        try {
+            sc = new Scanner(System.in);
+            fileInputStream = Files.newInputStream(Paths.get(sc.nextLine()));
+            fileOutputStream = Files.newOutputStream(Paths.get(sc.nextLine()));
+            int fileSize = fileInputStream.available();
+            byte[] buffer = new byte[fileSize];
+            fileInputStream.read(buffer);
+            int bufferCounter = 0;
+            while (bufferCounter < fileSize - 1) {
+                byte byte1 = buffer[bufferCounter];
+                byte byte2 = buffer[bufferCounter + 1];
+                buffer[bufferCounter] = byte2;
+                buffer[bufferCounter + 1] = byte1;
+                fileOutputStream.write(buffer, bufferCounter, 2);
+                bufferCounter += 2;
+            }
+            fileOutputStream.write(buffer, bufferCounter, fileSize - bufferCounter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (sc != null)
+                sc.close();
+            if (fileInputStream != null)
+                fileInputStream.close();
+            if (fileOutputStream != null)
+                fileOutputStream.close();
+        }
+
+        // Read the name of a text file from the console
+        // Then read characters from this file (use the readAllLines(Path) method of
+        // the Files class) and displays everything except periods, commas, and spaces.
+        try {
+            sc = new Scanner(System.in);
+            String fileName = sc.nextLine();
+            List<String> linesX = Files.readAllLines(Paths.get(fileName));
+            for (String lineX : linesX) {
+                System.out.println(lineX.replaceAll("[,. ]", ""));
+            }
+        } finally {
+            if (sc != null)
+                sc.close();
+        }
+
+        // Read a file name from the console. Read a set of numbers from the file.
+        // Display only the even numbers, sorted in ascending order.
+        BufferedReader br = null;
+        List<Integer> list = new ArrayList<>();
+        try {
+            sc = new Scanner(System.in);
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(sc.nextLine())));
+            String lineY;
+            while ((lineY = br.readLine()) != null) {
+                int num = Integer.parseInt(lineY);
+                if (num % 2 == 0)
+                    list.add(num);
+            }
+        } finally {
+            if (sc != null)
+                sc.close();
+            if (br != null)
+                br.close();
+        }
+        Collections.sort(list);
+        for (int i = 0; i < list.size(); i++)
+            System.out.println(list.get(i));
     }
 }
